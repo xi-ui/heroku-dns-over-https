b77000122d2f10d067f944935f17e4b54e958df7
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2020-04-22 18:16:53 -0500

    Update do-server.py

--- do-server.py
+++ do-server.py
@@ -7,21 +7,20 @@ PORT = int(sys.argv[1])
 class SimpleHandler(http.server.BaseHTTPRequestHandler):
 	def do_HEAD(self):
 		self.send_response(200)
-		self.send_header("Content-type", "text/html")
+		self.send_header("Content-type", "text/plain")
 		self.end_headers()
 	def do_GET(self):
 		self.send_response(200)
-		self.send_header("Content-type", "text/html")
+		self.send_header("Content-type", "text/plain")
 		self.end_headers()
-		print(self.wfile)
-		self.wfile.write("<html><head><title>Title goes here.</title></head>".encode("utf-8"))
-		self.wfile.write("<body><p>This is a test.</p>".encode("utf-8"))
-		# If someone went to "http://something.somewhere.net/foo/bar/",
-		# then s.path equals "/foo/bar/".
-		self.wfile.write(("<p>You accessed path: %s</p>" % self.path).encode("utf-8"))
-		self.wfile.write("</body></html>".encode("utf-8"))
+		self.wfile.write((self.path).encode("utf-8"))
 		self.wfile.close()
-
+	def do_POST(self):
+		self.send_response(400)
+		self.send_header("Content-type", "text/plain")
+		self.end_headers()
+		self.wfile.write("400 Bad Request".encode("utf-8"))
+		self.wfile.close()
 try:
 	server = http.server.HTTPServer(('0.0.0.0',PORT),SimpleHandler)
 	print('Started http server')
