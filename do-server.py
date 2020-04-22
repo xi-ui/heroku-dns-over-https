e8d9db2f896d0c33c5424a0d937a131605ea910e
WJP2003 <31232449+WJP2003@users.noreply.github.com>
2020-04-22 17:53:19 -0500

    Update do-server.py

--- do-server.py
+++ do-server.py
@@ -1,10 +1,31 @@
+import http.server
+import socketserver
 import sys
-from http.server import HTTPServer,BaseHTTPRequestHandler
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+PORT = sys.argv[1]
+
+class SimpleHandler(http.server.BaseHTTPRequestHandler):
+	def do_HEAD(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
 	def do_GET(self):
 		self.send_response(200)
+		self.send_header("Content-type", "text/html")
 		self.end_headers()
-		self.wfile.write(b'Hello, world!')
-		return self
-httpd = HTTPServer(('localhost',int(sys.argv[1])),SimpleHTTPRequestHandler)
-httpd.serve_forever()
+		print(self.wfile)
+		self.wfile.write("<html><head><title>Title goes here.</title></head>".encode("utf-8"))
+		self.wfile.write("<body><p>This is a test.</p>".encode("utf-8"))
+		# If someone went to "http://something.somewhere.net/foo/bar/",
+		# then s.path equals "/foo/bar/".
+		self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path))
+		self.wfile.write("</body></html>".encode("utf-8"))
+		self.wfile.close()
+
+try:
+	server = http.server.HTTPServer(('localhost',PORT),SimpleHandler)
+	print('Started http server')
+	server.serve_forever()
+except KeyboardInterrupt:
+	print('^C received, shutting down server')
+	server.socket.close()
